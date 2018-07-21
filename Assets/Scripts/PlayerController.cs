9f598dbc50535e533862b76653684be8deb5edee
Benr1312 <benjaminrobinson1313@gmail.com>
2018-07-21 17:19:18 +0800

    Pick ups and Text ui
    Added ability to pickup collectables.
    Added the ui for pickup count, added the win message.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+  private int count;
+
+  public Text countText;
+  public Text winText;
 	public float speed;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText();
+    winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -19,4 +27,19 @@ public class PlayerController : MonoBehaviour {
 	
 		rb.AddForce (movement * speed);
 	}
+
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Pickup")) {
+      other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText();
+    }
+  }
+
+  void SetCountText () {
+    countText.text = "Count: " + count.ToString();
+    if (count >= 8) {
+      winText.text = "You Win!";
+    }
+  }
 }
